2932895b38d66ef18c4636510d658866ad4fd6e5
aspizu <aspizu@protonmail.com>
2025-01-28 22:10:48 +0530

    refactor: remove debug print statements from dependency addition

--- src/project.rs
+++ src/project.rs
@@ -30,16 +30,11 @@ impl Project {
         }
         self.synced.insert(package.path.clone());
         let mut handles = vec![];
-        println!(
-            "adding dependencies from package {}",
-            package.path.display()
-        );
         for (name, url) in &package.manifest.dependencies {
             let name = name.clone();
             let url = url.clone();
             let packages_cache = self.packages_cache.clone();
             let backpack = self.backpack.clone();
-            println!("adding dependency {}", url);
             handles.push(thread::spawn(move || {
                 let path = packages_cache.lock().unwrap().get_package(url).unwrap();
                 backpack.lock().unwrap().add_package(name, &path).unwrap();
