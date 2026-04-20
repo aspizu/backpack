d95c8fbe757d839687ecdf5f1ad7224a47e5a1be
Priyanshu Dangare <hello@aspiz.uk>
2026-04-21 00:17:13 +0530

    init

--- src/backpack/cli/new.py
+++ src/backpack/cli/new.py
@@ -2,20 +2,32 @@ import importlib.resources
 import logging
 import shutil
 import sys
+from pathlib import Path
 from typing import TYPE_CHECKING
 
+from backpack.cmd import cmd
+
 if TYPE_CHECKING:
-    from pathlib import Path
+    from importlib.resources.abc import Traversable
 
 log = logging.getLogger(__name__)
 res = importlib.resources.files("backpack.res")
+
+
+def _copy_files(src: Traversable, dst: Path) -> None:
+    for path in src.iterdir():
+        if path.is_dir():
+            dst.joinpath(path.name).mkdir(parents=True, exist_ok=True)
+            _copy_files(path, dst.joinpath(path.name))
+        else:
+            shutil.copyfileobj(path.open("rb"), dst.joinpath(path.name).open("wb"))
 
 
 def new(name: Path) -> None:
     outdir = name.absolute()
     outdir.mkdir(parents=True, exist_ok=True)
     if len(list(outdir.iterdir())) > 0:
-        log.error("directory '%s' is not empty", outdir)
+        log.error("directory '%s' is not empty", outdir.relative_to(Path().absolute()))
         sys.exit(1)
-    for path in res.joinpath("template").iterdir():
-        shutil.copyfileobj(path.open("rb"), outdir.joinpath(path.name).open("wb"))
+    _copy_files(res.joinpath("template"), outdir)
+    cmd("git").args("init").run(cwd=outdir)
